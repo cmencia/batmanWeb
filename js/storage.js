d8175f8b3cd39745d735b994974e82f9ee9b2cbf
Carlos <Carlos@Carless-MacBook-Pro.local>
2017-01-08 23:47:28 +0100

    Added storaje.js

--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,11 @@
-if (typeof(Storage) !== 'undefined') {
+//Para almacenar datos del usuario en el navegador que se está utilizando. Se ALMACENA EN EL NAVEGADOR
+
+if (typeof(Storage) !== 'undefined') { //Navegador, tienes el objeto storage? Local no se pierde cuando cerramos ordenador
     localStorage.setItem("name", "Batman");
     console.log(localStorage.getItem("name"));
     //localStorage.removeItem("name");
 } else {
-    console.error("No dispones de webstorage");
+    console.error("No dispones de webstorage, instalalo");
 }
+
+//en sessionStorage se pierde la información al cerrar el navegador
